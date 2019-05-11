7eb3fa312dc1093eaf845dfe2c5cde9b98f14119
Maribel <minicatsCB@users.noreply.github.com>
2019-05-12 01:05:40 +0200

    Implement "Product" Bootstrap example (simplified)

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
+
 app.set("view engine", "pug");
 
 app.get("/", (req, res) => {
     res.render("index", {
-        title: "Test title",
-         message: "Hey there!"
+        title: "Express Server Render"
      });
 });
 
