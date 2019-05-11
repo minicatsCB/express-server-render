ee44483916cfa49b44e02b1ebc721ea4072dc306
Maribel <minicatsCB@users.noreply.github.com>
2019-05-11 21:10:00 +0200

    Render basic view with Pug

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "pug");
+
 app.get("/", (req, res) => {
-    res.send("Server is running!");
+    res.render("index", {
+        title: "Test title",
+         message: "Hey there!"
+     });
 });
 
 app.listen(port, () => console.log("Listening on port " + port));
